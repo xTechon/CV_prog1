17e90f7018eb9ea9c5bb431af10ab53b4c1ff741
W. Daniel Hiromoto <danielhiromoto@outlook.com>
2024-10-01 23:32:04 -0400

    swap filenames

--- Q3/q3_a1.py
+++ Q3/q3_a1.py
@@ -5,57 +5,18 @@ import matplotlib.pyplot as plt
 import scipy.ndimage as ndi
 import cv2 as cv
 
-# define a 5x5 gaussian kernel
-GKERNEL = np.array([[ 1, 4, 6, 4, 1 ],
-                         [ 4, 16, 24, 16, 4 ],
-                         [ 6, 24, 36, 24, 6],
-                         [ 4, 16, 24, 16, 4],
-                         [ 1, 4, 6, 4, 1]
-                        ])
-# normalize the kernel
-GKERNEL = GKERNEL / 256
-
-IMAGE_DEBUG = False
-
-# add plot to figure
-def addPlot(fig, position, img, title="", toggle=False):
-    if IMAGE_DEBUG is True or toggle is True:
-        fig["figure"].add_subplot(fig["row"], fig["column"], position)
-        plt.imshow(img)
-        plt.title(title)
-
-# wrapper around plt.show to globally toggle
-def display(toggle=False):
-    if IMAGE_DEBUG is True or toggle is True:
-        plt.show()
-
-# wrapper around create figure
-def createFigure(size, row, column, clear=False, toggle=False):
-    if IMAGE_DEBUG is True or toggle is True:
-        output = {
-            "figure": plt.figure(figsize=size, clear=toggle),
-            "row": row,
-            "column": column
-            }
-        return output
-    return None
-
-    
-
 # creates a composite image with original image on left and pyramid layers on right
 def compositeImage(pyramid):
     # original image dimensions
-    height, width, channels = pyramid[0].shape
+    height, width = pyramid[0].shape
 
     # determine the size of the composite image
     composite_height = max(height, sum(layer.shape[0] for layer in pyramid[1:]))
     composite_width = width + pyramid[1].shape[1]
-
-    # initalize composite image
-    output = np.zeros((composite_height, composite_width, channels), dtype=np.uint8)
+    output = np.zeros((composite_height, composite_width), dtype=np.uint8)
 
     # focus the original image on the left
-    output[:height, :width, :] = pyramid[0]
+    output[:height, :width] = pyramid[0]
 
     img_offset = 0
     for layer in pyramid[1:]:
@@ -65,194 +26,258 @@ def compositeImage(pyramid):
 
     return output
 
-# blur based on a kernel
-def gaussianBlur(src, mult=1):
+# function for applying a gaussian blur on an RGB image channel
+def gaussianBlur(channel, mult=1):
+    # define a 5x5 gaussian kernel
+    gaussian = np.array([[ 1, 4, 6, 4, 1 ],
+                         [ 4, 16, 24, 16, 4 ],
+                         [ 6, 24, 36, 24, 6],
+                         [ 4, 16, 24, 16, 4],
+                         [ 1, 4, 6, 4, 1]
+                        ])
+    # normalize the kernel
+    gaussian = gaussian / 256
+
     # scale the guassian by a factor mult
-    gaussian = GKERNEL * mult
+    gaussian = gaussian * mult
 
-    # split into channels
-    ch1 = src[:, :, 0]
-    ch2 = src[:, :, 1]
-    ch3 = src[:, :, 2]
+    # convolve image channel and gaussian kernel
+    output = ndi.convolve(channel, gaussian, mode='constant')
 
-    # convolve image and gaussian kernel
-    ch1 = ndi.convolve(ch1, gaussian, mode='nearest')
-    ch2 = ndi.convolve(ch2, gaussian, mode='nearest')
-    ch3 = ndi.convolve(ch3, gaussian, mode='nearest')
-
-    # recombine the image RGB channels
-    output = cv.merge((ch1, ch2, ch3))
-    return output
-
-# upsample image by 2
-def upsample(src):
-    height, width, _ = src.shape
-    output = np.zeros((2*height, 2*width, 3), dtype=np.uint8)
-    
-    # upsample
-    output[::2, ::2, :] = src
-    # blur image
-    output = gaussianBlur(output, 4)
     return output
 
 # reduces an image by half
 def reduceImg(src):
     # get every other y, x, pixel value
-    output = src[::2, ::2, :]
+    output = src[::2, ::2]
     return output
 
-# create an image's gaussian and laplacian pyramid
-def imgPyramids(src, levels=4):
+# upsample image channel by 2
+def upsample(src):
+    height, width = src.shape
+    output = np.zeros((2*height, 2*width), dtype=np.uint8)
+    
+    # upsample
+    output[::2, ::2] = src[:, :]
+    # blur image
+    #output = gaussianBlur(output)
+    output = 4 * cv.GaussianBlur(output, (5,5), 0)
+    return output
 
-    # create datastructure
-    output = { "gaussian": [], "laplacian": []}
 
-    # base layer of gaussian pyramid
-    output["gaussian"].append(copy.deepcopy(src))
+# creates a the image channel pyramids of levels from an image src
+def imgPyramids(src, levels=3):
+    
+    output = {"gaussian": [], "laplacian": []}
+    output["gaussian"].append(src)
 
-    # inital image
     current = src
-
-    # generate pyramid
     for _ in range(levels):
-        # new layer
+        # set the next layer
         gaussian = current
 
-        # smooth and reduce image
+        # Generate next Gaussian layer
         gaussian = gaussianBlur(gaussian)
         gaussian = reduceImg(gaussian)
 
-        # make sure image it's all ints
-        gaussian = np.rint(gaussian)
-        gaussian = np.array(gaussian, dtype=np.uint8)
+        output["gaussian"].append(copy.copy(gaussian))
 
-        # add new gaussian layer to pyramid
-        output["gaussian"].append(copy.deepcopy(gaussian))
+        # Generate next Laplacian layer
+        laplacian = upsample(gaussian)
+        laplacian = current - laplacian
 
-        # laplace layer upsample
-        laplace = upsample(gaussian)
-        # Gk - Gk+1
-        laplace = current - laplace
+        output["laplacian"].append(copy.copy(laplacian))
 
-        # make sure it's all ints
-        laplace = np.rint(laplace)
-        laplace = np.array(laplace, dtype=np.uint8)
-
-        # add laplace layer to pyramid
-        output["laplacian"].append(copy.deepcopy(laplace))
-
-        # itterate for next loop
         current = gaussian
     return output
 
-# blend the pyramids of two images and a Black and white mask
-def blendPyramids(white, black, mask):
-    # init output
-    output = {"gaussian": [], "laplacian": []}
-
-    # construct base layer
-    A = white["gaussian"][-1]
-    B = black["gaussian"][-1]
-    M = mask["gaussian"][-1] / 255
-    base = A * M + (1 - M) * B
-
-    # make sure base is all ints
-    base = np.rint(base)
-    base = np.array(base, dtype=np.uint8)
-
-    # add base gaussian layer
-    output["gaussian"].append(copy.deepcopy(base))
-
-    # blend laplacian layers
-    for layer, m in reversed(list(enumerate(mask["gaussian"][:-1]))):
-
-        # split the image channels
-        whiteChannels = cv.split(white["laplacian"][layer])
-        blackChannels = cv.split(black["laplacian"][layer])
-        maskChannels = cv.split(m)
-
-        laplaceChannels = []
-        for channel, mask in enumerate(maskChannels):
-            a = whiteChannels[channel]
-            b = blackChannels[channel]
-            #am = a*mask
-            difference = (255-mask)
-            out = a * (mask / 255) + (1 - (mask / 255)) * b
-
-            # make sure out is ints
-            out = np.rint(out)
-            out = np.array(out, dtype=np.uint8)
-            laplaceChannels.append(out)
-        
-        # merge the channels
-        laplace = cv.merge(laplaceChannels)
-
-        # add to pyramid
-        output["laplacian"].append(copy.deepcopy(laplace))
+# where src is an image pyramid
+def reconstructImg(src):
     
+    #rows = 1
+    #columns = 4
+    current = src["gaussian"][-1]
+    for layer in reversed(src["laplacian"]):
+        #print(current.shape)
+        temp = upsample(current)
+        
+        """
+        figure1 = plt.figure(figsize=(10,7), clear=True)
+        figure1.add_subplot(rows, columns, 1)
+        plt.imshow(current)
+        plt.title("current")
+        figure1.add_subplot(rows, columns, 2)
+        plt.imshow(copy.copy(temp))
+        plt.title("upsampled")
+        """
+
+        temp = temp + layer
+
+        """
+        figure1.add_subplot(rows, columns, 3)
+        plt.imshow(layer)
+        plt.title("laplace layer")
+        figure1.add_subplot(rows, columns, 4)
+        plt.imshow(temp)
+        plt.title("Addition")
+        plt.show()
+        """
+
+        current = temp
+    return current
+
+
+# lowest lv layer of both images
+# the laplacian pyramid of both images
+# gaussian pyramid + 1 size of images
+# each input is a pyramid "pair"
+def pyramidBlend(pyramidA, pyramidB, mask):
+
+    # create a new image pyramid
+    output = {"gaussian": [], "laplacian": []}
+    
+    base = mask.get("gaussian")[-1] / 255
+
+    # get the lowest layer of mask
+    current = (pyramidA["gaussian"][-1] * (base)) + ((1 - base) * pyramidB["gaussian"][-1])
+    
+    # current = np.zeros((base.shape[0], base.shape[1]))
+    
+    output["gaussian"].append(np.rint(current))
+    # mask * A + (1-mask) * B
+    for layer, m in reversed(list(enumerate(mask["gaussian"][:-1]))):
+        """
+        figure2 = plt.figure(figsize=(10,7), clear=True)
+        rows = 1
+        columns = 4
+        figure2.add_subplot(rows, columns, 1)
+        plt.imshow(pyramidA["laplacian"][layer])
+        plt.title("laplacian A")
+        figure2.add_subplot(rows, columns, 2)
+        plt.imshow(pyramidB["laplacian"][layer])
+        plt.title("laplacian B")
+        figure2.add_subplot(rows, columns, 3)
+        plt.imshow(m)
+        plt.title("mask")
+        """
+
+        l = (pyramidA["laplacian"][layer] * (m/255)) + ((1-(m/255)) * pyramidB["laplacian"][layer]) 
+        """
+        figure2.add_subplot(rows, columns, 4)
+        plt.imshow(l)
+        plt.title("blended layer")
+        plt.show()
+        """
+
+        # Reconstruct into blended pyramid
+        #temp = upsample(current)
+        #temp = temp + l
+        output["laplacian"].append(np.rint(l))
+
     # reverse order for consistency
     output["laplacian"] = output["laplacian"][::-1]
+    
+    return output
 
+def imageBlend(imgW, imgB, mask, layers=4):
+
+    # split the images
+    iwR, iwG, iwB = cv.split(imgW)
+    ibR, ibG, ibB = cv.split(imgB)
+    m, _, _ = cv.split(mask)
+
+    # create pyramid for the mask
+    pM = imgPyramids(m, layers)
+    
+    # create pyramids for the channels
+    pwR = imgPyramids(iwR, layers)
+    pwG = imgPyramids(iwG, layers)
+    pwB = imgPyramids(iwB, layers)
+
+    pbR = imgPyramids(ibR, layers)
+    pbG = imgPyramids(ibG, layers)
+    pbB = imgPyramids(ibB, layers)
+
+    # blend the pyramids
+    bR = pyramidBlend(pwR, pbR, pM)
+    bG = pyramidBlend(pwG, pbG, pM)
+    bB = pyramidBlend(pwB, pbB, pM)
+
+    # reconstruct each pyramid
+    outR = reconstructImg(bR)
+    outG = reconstructImg(bG)
+    outB = reconstructImg(bB)
+
+    """
+    figure2 = plt.figure(figsize=(10,7), clear=True)
+    rows = 1
+    columns = 4
+    figure2.add_subplot(rows, columns, 1)
+    plt.imshow(outR)
+    plt.title("Red")
+    figure2.add_subplot(rows, columns, 2)
+    plt.imshow(outG)
+    plt.title("Green")
+    figure2.add_subplot(rows, columns, 3)
+    plt.imshow(outB)
+    plt.title("Blue")
+    """
+    outR[outR < 0] = 0
+    outR[outR > 255] = 255
+    outR = outR.astype(np.uint8)
+    outG[outG < 0] = 0
+    outG[outG > 255] = 255
+    outG = outG.astype(np.uint8)
+    outB[outB < 0] = 0
+    outB[outB > 255] = 255
+    outB = outB.astype(np.uint8)
+
+    output = cv.merge((outR, outG, outB))
+
+    """
+    figure2.add_subplot(rows, columns, 4)
+    plt.imshow(output)
+    plt.title("output")
+    plt.show()
+    """
     return output
 
 
 
-
-# Reconstruct an image from it's pyramids
-def collapsePyramid(pyramid):
-    # get the top layer of gaussian pyramid
-    current = pyramid["gaussian"][-1]
-
-    for laplace in reversed(pyramid["laplacian"]):
-        figure2 = createFigure((10,7), 1, 4, True)
-
-        addPlot(figure2, 1, current, "layer")
-
-        # upsample the current layer
-        current = upsample(current)
-
-        addPlot(figure2, 2, current, "upsampled")
-        addPlot(figure2, 3, laplace, "laplace")
-
-        # add the missing details
-        current = current + laplace
-        addPlot(figure2, 4, current, "result")
-        display()
-
-    # make sure current is an np int array
-    current = np.rint(current)
-    current = np.array(current, dtype=np.uint8)
-    return current
-
-#################
-# MAIN FUNCTION
-#################
-
-
+# load image
 face1 = iio.imread('./face256.png')
-face2 = iio.imread('./2face256.png')
-mask = iio.imread('./2Mask256.png')
+fruit1 = iio.imread('./2face256.png')
+mask1 = iio.imread('./2Mask256.png')
 
-face1 = np.array(face1, dtype=np.uint8)
-face2 = np.array(face2, dtype=np.uint8)
-mask = np.array(mask, dtype=np.uint8)
+working_img1 = fruit1
+working_img2 = face1
+working_img3 = mask1
 
-face1_pyramid = imgPyramids(face1)
-face2_pyramid = imgPyramids(face2)
-mask_pyramid = imgPyramids(mask)
+out1 = imageBlend(working_img1, working_img2, working_img3, 4)
 
-blended_pyramid = blendPyramids(face2_pyramid, face1_pyramid, mask_pyramid)
-composite = compositeImage(blended_pyramid["laplacian"])
-
-result = collapsePyramid(blended_pyramid)
+# Plotting setup
+figure = plt.figure(figsize=(10,7))
+rows = 1
+columns = 4
 
 
-# plotting setup
-figure = createFigure((20,7), 1, 4, toggle=True)
+figure.add_subplot(rows, columns, 1)
+plt.imshow(working_img1)
+plt.title("Operand 1")
 
-addPlot(figure, 1, face1, "face 1", True)
-addPlot(figure, 2, face2, "face 2", True)
-addPlot(figure, 3, mask, "mask", True)
-addPlot(figure, 4, result, "result", True)
+figure.add_subplot(rows, columns, 2)
+plt.imshow(working_img2)
+plt.title("Operand 2")
+
+
+figure.add_subplot(rows, columns, 3)
+plt.imshow(working_img3)
+plt.title("Mask")
+
+
+figure.add_subplot(rows, columns, 4)
+plt.imshow(out1)
+plt.title("Result")
+
 
 plt.show()
